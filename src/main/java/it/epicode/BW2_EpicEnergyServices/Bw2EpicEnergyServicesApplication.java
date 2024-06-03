9524acdad8b70ea00d0f4b1d59ec440ece0708c1
stefortezza <stefano.fortezza98@hotmail.it>
2024-06-03 12:47:59 +0200

    PrimoCommit

---
+++ src/main/java/it/epicode/BW2_EpicEnergyServices/Bw2EpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.BW2_EpicEnergyServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw2EpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw2EpicEnergyServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/BW2_EpicEnergyServices/Bw2EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.BW2_EpicEnergyServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw2EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
