43d6fae04505d5abef441779cf3a56aa01dc1a8b
stefortezza <stefano.fortezza98@hotmail.it>
2024-06-03 17:05:41 +0200

    creazione Repo - Security and Service

---
+++ src/main/java/it/epicode/BW2_EpicEnergyServices/Repository/TownRepository.java
@@ -0,0 +1,6 @@
+package it.epicode.BW2_EpicEnergyServices.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Integer> {
+}
